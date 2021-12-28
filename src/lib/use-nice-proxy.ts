36718fcc3734e6cc47b448bb6a9cbbee49918ad1
杨涛 <807081817@qq.com>
2021-12-28 17:05:46 +0800

    移除log

--- src/lib/use-nice-proxy.ts
+++ src/lib/use-nice-proxy.ts
@@ -6,14 +6,9 @@ const getContent = filename => {
 }
 
 export const useNiceProxy = (req) => {
-  console.log(Date.now())
-
   try {
     const config = JSON.parse(getContent('proxy-config.json'))
     const list = JSON.parse(getContent('proxy-list.json'))
-
-    console.log(config)
-    console.log(list)
 
     return Reflect.get(list, config.target)
   } catch (err) {
